670c8388f6d95b4b6996dd3d7021206bd0c4ea7d
calebrori <32507245+calebrori@users.noreply.github.com>
2023-08-04 11:09:20 +0300

    4-TCP_and_UDP_ports.sh

--- 0x07-networking_basics/4-TCP_and_UDP_ports.sh
+++ 0x07-networking_basics/4-TCP_and_UDP_ports.sh
@@ -1,32 +1,37 @@
 #!/usr/bin/env bash
 
-# Get the list of listening sockets and corresponding PIDs
-listening_sockets=$(netstat -tlnp 2>/dev/null | grep 'LISTEN')
-
-# Check if netstat command is available, otherwise use ss command (Linux)
-if [ -z "$listening_sockets" ]; then
-    listening_sockets=$(ss -tlnp 2>/dev/null | grep 'LISTEN')
+# Check if the script is executed with superuser privileges
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root (or using sudo)."
+  exit 1
 fi
 
-# If neither netstat nor ss command is available, exit
-if [ -z "$listening_sockets" ]; then
-    echo "Error: Unaivalable listening ports. Confirm 'netstat' or 'ss' commands are available."
-    exit 1
-fi
+# Function to get program name from PID
+get_program_name() {
+  local pid=$1
+  local program_name=$(ps -p "$pid" -o comm= 2>/dev/null)
+  echo "$program_name"
+}
 
 # Print header
-echo "Listening Ports | PID | Process Name"
+echo "Active Internet connections (only servers)"
+echo "Proto Recv-Q Send-Q Local Address           Foreign Address         State       PID/Program name"
 
-# Loop through each listening socket
-while read -r line; do
-    port=$(echo "$line" | awk '{print $4}' | awk -F ":" '{print $2}')
-    pid=$(echo "$line" | awk '{print $7}' | awk -F "/" '{print $1}')
-    process_name=$(echo "$line" | awk '{print $7}' | awk -F "/" '{print $2}')
+# Get listening ports with associated PIDs and program names using netstat
+netstat -tulnep | awk '$6 == "LISTEN" && $1 ~ /^(tcp|udp)/ {
+  split($4, local_addr, ":");
+  pid = substr($NF, 0, index($NF, "/")-1);
+  program_name = get_program_name(pid);
+  printf "%-4s %-7s %-7s %-24s %-22s %-12s %s\n", $1, $2, $3, $4, $5, $6, pid "/" program_name;
+}'
 
-    # If PID is numeric, get the process name using 'ps' command
-    if [[ $pid =~ ^[0-9]+$ ]]; then
-        process_name=$(ps -p "$pid" -o comm=)
-    fi
+# Print footer
+echo "Active UNIX domain sockets (only servers)"
+echo "Proto RefCnt Flags       Type       State         I-Node   PID/Program name    Path"
 
-    echo "$port | $pid | $process_name"
-done <<< "$listening_sockets"
+# Get listening UNIX domain sockets with associated PIDs and program names using netstat
+netstat -lxnep | awk '$5 == "LISTEN" {
+  pid = substr($NF, 0, index($NF, "/")-1);
+  program_name = get_program_name(pid);
+  printf "%-4s %-8s %-11s %-11s %-12s %-8s %s\n", $1, $2, $3, $4, $5, pid "/" program_name, $9;
+}'
