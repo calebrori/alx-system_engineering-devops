f1944c52d68db888036ac234d39a19e0d3331404
calebrori <calebrori@gmail.com>
2023-09-22 18:31:09 +0000

    0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+#Creates file school inside tmp dir
+file { '/tmp/school':
+  ensure	=>  file,
+  content	=>  'I love Puppet',
+  mode		=>  '07744',
+  owner		=>  'www-data',
+  group		=>  'www-data',
+}
