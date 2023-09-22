6a8abf5ea73fb878a7256a1d88f8a5bbba8fd467
calebrori <32507245+calebrori@users.noreply.github.com>
2023-09-22 22:19:36 +0300

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 #Installs puppet-lint package
 
 package { 'flask':
-  ensure    =>  '2.1.0',
-  provider  =>  'pip3',
- }
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
