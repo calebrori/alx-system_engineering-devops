fe159e766416f59e2396fe9025e93fbbca47e9c1
calebrori <32507245+calebrori@users.noreply.github.com>
2023-09-23 18:34:54 +0300

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 #Script that connects to server without typing a password
 
-file { 'etc/ssh/ssh_config':
-  ensure => present,
- }
- 
- file_line { 'Turn off passwd auth':
+file_line{'alias name':
   path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
-  replace => true,
- }
- 
-  file_line { 'Declare identity file':
-   path => '/etc/ssh/ssh_config',
-   line => 'IdentityFile ~/.ssh/school'
-   replace => true,
- }
+  line => 'Host 100.26.212.133',
+}
+file_line{'host name':
+  path => '/etc/ssh/ssh_config',
+  line => '    HostName 100.26.212.133',
+}
+file_line{'user name':
+  path => '/etc/ssh/ssh_config',
+  line => '    User ubuntu',
+}
+file_line{'identity file':
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/school',
+}
+file_line{'Turn off passwd authantication':
+  path => '/etc/ssh/ssh_config',
+  line => '    PasswordAuthentication no',
+}
