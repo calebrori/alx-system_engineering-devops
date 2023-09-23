d1bcf5cd7a55a317b61fc0cbbe3eb534e7df838f
calebrori <32507245+calebrori@users.noreply.github.com>
2023-09-23 18:29:16 +0300

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -6,13 +6,11 @@ file { 'etc/ssh/ssh_config':
  }
  
  file_line { 'Turn off passwd auth':
-   path    => '/etc/ssh/ssh_config',
-   line    => 'PasswordAuthentication no',
-   match   => '^#PasswordAuthentication',
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
  }
  
   file_line { 'Declare identity file':
-   path    => '/etc/ssh/ssh_config',
-   line    => 'IdentityFile ~/.ssh/school',
-   match   => '^#IdentityFile',
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school'
  }
